735d94c1ff2c0b76ebf2bd4017c3d52673f55306
brandoncaulfield <19891236+brandoncaulfield@users.noreply.github.com>
2021-08-09 22:06:45 +0100

    sdfsdf

---
+++ helper.py
@@ -0,0 +1,245 @@
+import os
+import json
+import uuid
+import time
+
+from requests.api import get
+from kraken import *
+from datetime import date, datetime, timedelta
+import logging
+import pandas as pd
+
+
+def is_coin_above_50_day_sma(coin, interval=1440):
+    # ohlc_data = get_ohlc_data(coin, interval)
+    # df = pd.DataFrame.from_dict(ohlc_data['result'])
+    # df['sma50'] = 
+    return True
+
+def is_coin_above_100_day_sma(coin):
+    return True
+
+def is_coin_above_200_day_sma(coin):
+    return True
+
+
+def get_order_log():
+
+    with open('order_log.json', 'r') as file:
+        order_log = json.load(file)
+    
+    return order_log
+
+
+def doesFileExists(filePathAndName):
+    return os.path.exists(filePathAndName)
+
+
+def get_current_price(coin):
+    current_price = get_kraken_price(coin)
+    return current_price["result"][coin]["c"][0]
+
+
+def get_previouse_price(coin):
+    with open('previous_price.json', 'r') as file:
+        try:
+            previous_price = json.load(file)
+        except:
+            previous_price = {}
+    if len(previous_price) < 1:
+        current_price = get_current_price(coin)
+        if len(current_price["error"]) == 0:
+            with open('previous_price.json', 'w') as file:
+                previous_price[coin] = float(current_price)
+                if len(previous_price) > 0:
+                    json.dump(previous_price, file)
+                    return previous_price
+                # else:
+                #     previous_price = {}
+                #     json.dump(previous_price, file)
+
+    else:
+        return previous_price
+
+
+def do_we_own_the_coin(coin):
+    with open('portfolio.json', 'r') as file:
+        portfolio = json.load(file)
+    if coin in portfolio:
+        return portfolio
+    else:
+        return False
+
+
+def get_asset_balance(asset):
+    
+    account_data = get_account_balance()
+
+    try:
+        asset_amount = account_data['result'][asset]
+        return asset_amount
+    except:
+        print(f'Could not find {asset} in account balance')
+        logging.error(f'Could not find {asset} in account balance')
+        return 0
+
+
+def place_order(coin, order_type, volume):
+
+    order_resp = add_kraken_order(coin, order_type, volume)
+    if len(order_resp["error"]) == 0:
+        return order_resp
+    else:
+        raise Exception(order_resp) 
+
+def get_portfolio_data(coin):
+    
+    with open('portfolio.json', 'r') as file:
+        portfolio_data = json.load(file)
+    
+    return portfolio_data[coin]
+
+
+def add_coin_to_portfolio(coin, portfolio_data):
+
+    if not doesFileExists('./portfolio.json'):
+        with open('portfolio.json', 'w') as file:
+            json.dump({}, file)
+    
+    with open('portfolio.json', 'r') as file:
+        current_portfolio_data = json.load(file)
+    
+    # Add the new data to the current portfolio data
+    current_portfolio_data[coin] = portfolio_data[coin]
+
+    # Update the portfolio
+    with open('portfolio.json', 'w') as file:
+        try:
+            json.dump(current_portfolio_data, file)
+            print(f'{coin} added to portfolio at {str(datetime.now())} length:{len(current_portfolio_data)}')
+            logging.info(f'{coin} added to portfolio at {str(datetime.now())} length:{len(current_portfolio_data)}')
+        except:
+            # If anything happens try protect the integrity of the json file
+            json.dump({}, file)
+            print(f'There was an issue adding {coin} to the portfolio :(')
+            logging.error(f'There was an issue adding {coin} to the portfolio :(')
+
+    # return coin + " added to portfolio"
+
+
+def remove_coin_from_portfolio(coin):
+
+    with open('portfolio.json', 'r') as file:
+        portfolio_data = json.load(file)
+
+    with open('portfolio.json', 'w') as file:
+        try:
+            del portfolio_data[coin]
+
+            if len(portfolio_data) > 0:
+                json.dump(portfolio_data, file)
+                print(f'{coin} removed from portfolio at {str(datetime.now())} length:{len(portfolio_data)}')
+                logging.info(f'{coin} removed from portfolio at {str(datetime.now())} length:{len(portfolio_data)}')
+            else:
+                json.dump({}, file)
+        except:
+            print(f"Issue while deleting {coin} from portfolio")
+            logging.error(f"Issue while deleting {coin} from portfolio")
+
+
+def determine_volume_to_buy(current_price, value_to_spend, currency, safe_percentage=False):
+
+    account_balance = get_account_balance()
+
+    # Handle scenario where we have less than the user specified parameter in 
+    # our account, still make the purchase with whatever we have.
+    # safe_percentage is there in case the price 
+    # changes and the order fails due to insufficient funds
+    if account_balance != 0:
+        if float(account_balance['result'][currency]) <= float(value_to_spend):
+            volume_to_buy = float(account_balance['result'][currency]) / float(current_price)
+
+            if safe_percentage:
+                reduced_volume_to_buy = (float(volume_to_buy) / value_to_spend) * float(safe_percentage)
+                return reduced_volume_to_buy
+            else:
+                return volume_to_buy
+
+        else:
+            volume_to_buy = float(account_balance['result'][currency]) / float(current_price)
+
+            if safe_percentage:
+                reduced_volume_to_buy = (float(account_balance['result'][currency]) / value_to_spend) * float(safe_percentage)
+                return reduced_volume_to_buy
+            else:
+                return volume_to_buy 
+
+
+def determine_volume_to_sell(coin):
+
+    with open('portfolio.json', 'r') as file:
+        portfolio_data = json.load(file)
+    
+    return portfolio_data[coin]["vol"]
+
+
+
+def determine_take_profit(current_price, take_profit):
+
+    return float(current_price) + (float(current_price) / 100 * float(take_profit))
+
+
+def determine_stop_loss(current_price, stop_loss):
+
+    return float(current_price) - (float(current_price) / 100 * float(stop_loss))
+
+
+def update_order_log(order):
+
+    if not doesFileExists('./order_log.json'):
+        with open('order_log.json', 'w') as file:
+            json.dump({}, file)
+
+    # We have to open it first before adding the data
+    with open('order_log.json', 'r') as file:
+        order_log = json.load(file)
+
+    # We have ot open it again to write the data
+    with open('order_log.json', 'w') as file:
+        try:
+            # Generate random log ID
+            log_id = uuid.uuid1()
+            order_log[int(log_id)] = order
+            if len(order_log) > 0:
+                json.dump(order_log, file)
+            print("New entry added to Order Log!")
+            logging.info(f"New entry {log_id} added to Order Log!")
+        except:
+            print("Something happened while tryin to update the trading log :(")
+            logging.error("Something happened while tryin to update the trading log :( - id: {log_id}")
+
+
+def update_previous_price(coin, current_price):
+
+    if not doesFileExists('./previous_price.json'):
+        with open('previous_price.json', 'w') as file:
+            json.dump({}, file)
+
+    #  Get the old price
+    previous_price = get_previouse_price(coin)
+
+    with open('previous_price.json', 'w') as file:
+        try:
+            # Update the old price with the current price 
+            previous_price[coin] = current_price
+            print(f'updated previous price to: {previous_price} at {str(datetime.now())}')
+            logging.info(f'updated previous price to: {previous_price} at {str(datetime.now())}')
+            json.dump(previous_price, file)
+        except:
+            previous_price = {}
+            json.dump(previous_price, file)
+            logging.error(f'Issue updating previous price to current price: {current_price}')
+
+
+if __name__ == "__main__":
+    is_coin_above_50_day_sma("XXBTZUSD")
---
+++ kraken.py
@@ -0,0 +1,130 @@
+import os
+import time
+import json
+import requests
+
+import urllib.parse
+import hashlib
+import hmac
+import base64
+
+
+# Read Kraken API key and secret stored in environment variables
+api_url = "https://api.kraken.com"
+api_key = os.environ['API_KEY_KRAKEN']
+api_sec = os.environ['API_SEC_KRAKEN']
+
+
+def get_kraken_signature(urlpath, data, secret):
+
+    postdata = urllib.parse.urlencode(data)
+    encoded = (str(data['nonce']) + postdata).encode()
+    message = urlpath.encode() + hashlib.sha256(encoded).digest()
+
+    mac = hmac.new(base64.b64decode(secret), message, hashlib.sha512)
+    sigdigest = base64.b64encode(mac.digest())
+    return sigdigest.decode()
+
+
+# Attaches auth headers and returns results of a POST request
+def kraken_request(uri_path, data, api_key, api_sec):
+    headers = {}
+    headers['API-Key'] = api_key
+    # get_kraken_signature() as defined in the 'Authentication' section
+    headers['API-Sign'] = get_kraken_signature(uri_path, data, api_sec)
+    req = requests.post((api_url + uri_path), headers=headers, data=data)
+    return req
+
+
+def add_kraken_order(coin, order_type, volume):
+    try:
+        # Construct the request and print the result
+        resp = kraken_request('/0/private/AddOrder', {
+            "nonce": str(int(1000*time.time())),
+            "ordertype": "market",
+            "type": order_type,
+            "volume": volume,
+            "pair": coin,
+            # "price": 27500
+        }, api_key, api_sec)
+
+        return json.loads(resp.text)
+
+    except:
+        return "Error while placing buy order"
+
+
+def get_trade_details(trade_id):
+    # Expects trade ID
+    try:
+        resp = kraken_request('/0/private/QueryTrades', {
+            "nonce": str(int(1000*time.time())),
+            "txid": trade_id,
+            "trades": True
+        }, api_key, api_sec)
+
+        return json.loads(resp.text)
+    except:
+        print("Erro while getting kraken trade details")
+        return "Error while getting trade info"
+
+
+def get_kraken_price(coin):
+    url = 'https://api.kraken.com/0/public/Ticker?pair=' + coin
+    response = requests.get(url)
+    current_prices = response.json()
+    # if len(current_prices["error"]) == 0:
+    #     return current_prices['result']
+    # else:
+    return current_prices
+
+def get_order_info(txid):
+    try:
+        resp = kraken_request('/0/private/QueryOrders', {
+            "nonce": str(int(1000*time.time())),
+            "txid": txid,
+            "trades": True
+        }, api_key, api_sec)
+
+        return json.loads(resp.text)
+    except:
+        print("Erro while getting kraken order details")
+        return "Error while getting trade info"
+
+
+def get_ohlc_data(coin, interval):
+    url = 'https://api.kraken.com/0/public/OHLC?pair=' + coin + '&interval=' + str(interval)
+    response = requests.get(url)
+    ohlc_data = response.json()
+    return ohlc_data
+
+
+def get_account_balance():
+    try:
+        resp = kraken_request('/0/private/Balance', {
+            "nonce": str(int(1000*time.time()))
+        }, api_key, api_sec)
+
+        return json.loads(resp.text)
+    except:
+        print("Erro while getting kraken account balance")
+        return "Error while getting account balance"
+
+
+def run(order_type):
+    # resp = get_order_info("O4YDSC-R4ZXR-SGJDBX")
+    # if len(resp["error"]) == 0:
+    #     print(resp)
+    # else:
+    #     print(len(resp["error"]))
+    #     print(resp["error"])
+    resp = add_kraken_order("XETHZUSD", "sell", 0.02786793)
+    if resp:
+        print(resp)
+    # resp = get_account_balance()
+    # if resp:
+    #     print(resp)
+
+
+if __name__ == "__main__":
+    run("buy")
